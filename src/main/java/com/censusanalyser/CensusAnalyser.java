89ce6be6248415c950662ac6228ff85058e04c92
kundan kumar <kundanchauhan1997@gmail.com>
2023-03-29 14:54:13 +0530

    Added: Welcome Message

---
+++ src/main/java/com/censusanalyser/CensusAnalyser.java
@@ -0,0 +1,4 @@
+package com.censusanalyser;
+
+public class CensusAnalyser {
+}
---
+++ src/main/java/com/censusanalyser/WriteCsvData.java
@@ -0,0 +1,9 @@
+package com.censusanalyser;
+
+
+public class WriteCsvData {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Census Analyser Of Indian state");
+    }
+
+}
